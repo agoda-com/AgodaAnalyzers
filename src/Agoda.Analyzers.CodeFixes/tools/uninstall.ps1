2f48314b2b056ea8af682796f457e45f75e8dd35
Joel Dickson <j.dickson@hosted-solutions.com.au>
2017-01-15 18:39:31 +0700

    add nuspec file for packaging

---
+++ src/Agoda.Analyzers.CodeFixes/tools/uninstall.ps1
@@ -0,0 +1,25 @@
+﻿param($installPath, $toolsPath, $package, $project)
+# https://johnkoerner.com/csharp/creating-a-nuget-package-for-your-analyzer/
+
+$analyzersPath = join-path $toolsPath "analyzers"
+
+# Install the language agnostic analyzers.
+foreach ($analyzerFilePath in Get-ChildItem $analyzersPath -Filter *.dll)
+{
+    if($project.Object.AnalyzerReferences)
+    {
+        $project.Object.AnalyzerReferences.Remove($analyzerFilePath.FullName)
+    }
+}
+
+# Install language specific analyzers.
+# $project.Type gives the language name like (C# or VB.NET)
+$languageAnalyzersPath = join-path $analyzersPath $project.Type
+
+foreach ($analyzerFilePath in Get-ChildItem $languageAnalyzersPath -Filter *.dll)
+{
+    if($project.Object.AnalyzerReferences)
+    {
+        $project.Object.AnalyzerReferences.Remove($analyzerFilePath.FullName)
+    }
+}
