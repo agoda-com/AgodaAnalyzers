2bea89a3e478d39e7c5c739fd11c7e776994a881
AGODA\kbodrogi <krisztian.bodrogi@agoda.com>
2018-02-07 15:41:17 +0700

    Add new html based rule description

--- src/Agoda.Analyzers/AgodaCustom/AG0003HttpContextCannotBePassedAsMethodArgument.cs
+++ src/Agoda.Analyzers/AgodaCustom/AG0003HttpContextCannotBePassedAsMethodArgument.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Immutable;
+﻿using Agoda.Analyzers.Helpers;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using StyleCop.Analyzers;
+using System;
+using System.Collections.Immutable;
 
 namespace Agoda.Analyzers.AgodaCustom
 {
@@ -14,7 +15,7 @@ namespace Agoda.Analyzers.AgodaCustom
         public const string DiagnosticId = "AG0003";
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(CustomRulesResources.AG0003Title), CustomRulesResources.ResourceManager, typeof(CustomRulesResources));
         private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(CustomRulesResources.AG0003MessageFormat), CustomRulesResources.ResourceManager, typeof(CustomRulesResources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(CustomRulesResources.AG0003Description), CustomRulesResources.ResourceManager, typeof(CustomRulesResources));
+        private static readonly LocalizableString Description = DescriptionContentLoader.GetAnalyzerDescription(nameof(AG0003HttpContextCannotBePassedAsMethodArgument));
 
         private static readonly DiagnosticDescriptor Descriptor =
             new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerCategory.CustomQualityRules,
---
+++ src/Agoda.Analyzers/Helpers/DescriptionContentLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Agoda.Analyzers.Helpers
+{
+    public static class DescriptionContentLoader
+    {
+        /// <summary>
+        /// Load analyzer html description from the project
+        /// </summary>
+        /// <param name="analyzerName">Name of the analyzer.</param>
+        /// <returns>Content of the analyzer description.</returns>
+        public static string GetAnalyzerDescription(string analyzerName)
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), $"RuleContent\\{analyzerName}.html");
+            var content = File.ReadAllText(path);
+            return content;
+        }
+    }
+}
