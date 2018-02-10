6c742ff7bec76b2d9629866dd861f7e68729af21
AGODA\kbodrogi <krisztian.bodrogi@agoda.com>
2018-02-10 08:19:23 +0700

    Add example Analyzer & CodeFixer

---
+++ src/Agoda.Analyzers.CodeFixes/AgodaCustom/IfBracesAnalyzerCodeFixProvider.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Agoda.Analyzers.CodeFixes.AgodaCustom
+{
+    [ExportCodeFixProvider("IfBracesAnalyzerCodeFixProvider", LanguageNames.CSharp), Shared]
+    public class IfBracesAnalyzerCodeFixProvider : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create("IfBracesAnalyzer");
+
+        public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+
+        private async Task<Document> AddBracesAsync(Document document,
+            IfStatementSyntax ifStatement, CancellationToken cancellationToken)
+        {
+            var nonBlockStatement = ifStatement.Statement as ExpressionStatementSyntax;
+
+            var newBlockStatement = SyntaxFactory.Block(nonBlockStatement)
+                .WithAdditionalAnnotations(Formatter.Annotation);
+
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var newRoot = root.ReplaceNode(nonBlockStatement, newBlockStatement);
+
+            var newDocument = document.WithSyntaxRoot(newRoot);
+            return newDocument;
+        }
+
+        public async override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            // Find the type declaration identified by the diagnostic.
+            var declaration = root
+                .FindToken(diagnosticSpan.Start).Parent
+                .AncestorsAndSelf()
+                .OfType<IfStatementSyntax>()
+                .First();
+
+            // Register a code action that will invoke the fix.
+            context.RegisterCodeFix(
+                CodeAction.Create("Add braces",
+                c => AddBracesAsync(context.Document, declaration, c)),
+                diagnostic);
+        }
+    }
+}
---
+++ src/Agoda.Analyzers/AgodaCustom/IfBracesAnalyzer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Agoda.Analyzers.AgodaCustom
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class IfBracesAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "IfBracesAnalyzer";
+        internal const string Title = "If statement need braces";
+        internal const string MessageFormat = "If statement should be enclosed in braces";
+        internal const string Category = "Syntax";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title,
+            MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeSyntax, SyntaxKind.IfStatement);
+        }
+
+        private static void AnalyzeSyntax(SyntaxNodeAnalysisContext context)
+        {
+            var ifStatement = (IfStatementSyntax)context.Node;
+
+            if (ifStatement.Statement.IsKind(SyntaxKind.Block)) return;
+
+            var nonBlockStatement = ifStatement.Statement as ExpressionStatementSyntax;
+            if (nonBlockStatement == null) return;
+
+            var diagnostic = Diagnostic.Create(Rule, nonBlockStatement.GetLocation());
+            context.ReportDiagnostic(diagnostic);
+        }
+    }
+}
