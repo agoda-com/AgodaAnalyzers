7202c072ce769dd7f97b9aa37b749468e02532dc
Susakpol <susakpol.kunvuttivanit@agoda.com>
2018-09-06 17:48:21 +0700

    AG0018: Ensure that publicly exposed IEnumerable types are on our whitelist

---
+++ src/Agoda.Analyzers.Test/AgodaCustom/AG0018UnitTest.cs
@@ -0,0 +1,71 @@
+﻿// <copyright file="AG018Unittest.cs" company="Agoda Company Co., Ltd.">
+// AGODA ® is a registered trademark of AGIP LLC, used under license by Agoda Company Co., Ltd.. Agoda is part of Priceline (NASDAQ:PCLN)
+// </copyright>
+namespace Agoda.Analyzers.Test.AgodaCustom
+{
+    using Agoda.Analyzers.AgodaCustom;
+    using Agoda.Analyzers.Test.Helpers;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class AG0018UnitTest : DiagnosticVerifier
+    {
+        [Test]
+        public async Task AG0018_WhenCreateAMethodWhichReturnList_ShouldReturnAsAnInterface()
+        {
+            var code = $@"
+                        namespace Tests
+                        {{
+                            using System.Collections.Generic;
+                            using System.Collections.ObjectModel;
+                            public class IServerNamesProvider
+                            {{
+                                public List<double> LocalServer {{ get; set; }}
+                                public IList<double> LocalServerV2 {{ get; set; }}
+                                public HashSet<int> GetServerSet() {{ return null; }}
+                                public ISet<int> GetServerSetV2() {{ return null; }}
+                                public List<int> GetServerCounts() {{ return null; }}
+                                public IList<string> GetServerFullNames() {{ return null; }}        
+                                public Dictionary<string, string> GetServerDns() {{ return null; }}
+                                public IDictionary<string, string> GetServerDnsV2() {{ return null; }}
+                                public ReadOnlyDictionary<string, int> GetNumberOfServer() {{ return null; }}        
+                                public IReadOnlyDictionary<string, int> GetNumberOfServerV2() {{ return null; }}
+                                public KeyedCollection<string, string> GetServerDnsV3() {{ return null; }}
+                            }}
+                        }}";
+
+            var nUnit = MetadataReference.CreateFromFile(typeof(TestFixtureAttribute).Assembly.Location);
+
+            var doc = CreateProject(new[] { code })
+                .AddMetadataReference(nUnit)
+                .Documents
+                .First();
+
+            var analyzersArray = GetCSharpDiagnosticAnalyzers().ToImmutableArray();
+
+            var diag = await GetSortedDiagnosticsFromDocumentsAsync(analyzersArray, new[] { doc }, CancellationToken.None).ConfigureAwait(false);
+            var baseResult = CSharpDiagnostic(AG0018EnsureThatPubliclyExposedIEnumerableTypes.DiagnosticId);
+
+            VerifyDiagnosticResults(diag, analyzersArray, new[]
+            {
+                baseResult.WithLocation(8, 33),
+                baseResult.WithLocation(10, 33),
+                baseResult.WithLocation(12, 33),
+                baseResult.WithLocation(14, 33),
+                baseResult.WithLocation(16, 33),
+            });
+        }
+
+        protected override IEnumerable<DiagnosticAnalyzer> GetCSharpDiagnosticAnalyzers()
+        {
+            yield return new AG0018EnsureThatPubliclyExposedIEnumerableTypes();
+        }
+    }
+}
---
+++ src/Agoda.Analyzers/AgodaCustom/AG0018EnsureThatPubliclyExposedIEnumerableTypes .cs
@@ -0,0 +1,97 @@
+﻿// <copyright file="AG0018EnsureThatPubliclyExposedIEnumerableTypes.cs" company="Agoda Company Co., Ltd.">
+// AGODA ® is a registered trademark of AGIP LLC, used under license by Agoda Company Co., Ltd.. Agoda is part of Priceline (NASDAQ:PCLN)
+// </copyright>
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Agoda.Analyzers.Helpers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Agoda.Analyzers.AgodaCustom
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AG0018EnsureThatPubliclyExposedIEnumerableTypes : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "AG0018";
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(CustomRulesResources.AG0018Title), CustomRulesResources.ResourceManager, typeof(CustomRulesResources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(CustomRulesResources.AG0018Title), CustomRulesResources.ResourceManager, typeof(CustomRulesResources));
+        private static readonly LocalizableString Description = DescriptionContentLoader.GetAnalyzerDescription(nameof(AG0018EnsureThatPubliclyExposedIEnumerableTypes));
+
+        private static readonly DiagnosticDescriptor Descriptor =
+          new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerCategory.CustomQualityRules,
+              DiagnosticSeverity.Warning, AnalyzerConstants.EnabledByDefault, Description, null, WellKnownDiagnosticTags.EditAndContinue);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Descriptor);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeReturnType, SyntaxKind.MethodDeclaration);
+            context.RegisterSyntaxNodeAction(AnalyzePropertyType, SyntaxKind.PropertyDeclaration);
+        }
+
+        private void AnalyzeReturnType(SyntaxNodeAnalysisContext context)
+        {
+            var methodDeclaration = (MethodDeclarationSyntax)context.Node;
+
+            if (IsPubliclyExposedIEnumerableTypes(context)) { return; }
+
+            context.ReportDiagnostic(Diagnostic.Create(Descriptor, methodDeclaration.GetLocation()));
+        }
+
+        private void AnalyzePropertyType(SyntaxNodeAnalysisContext context)
+        {
+            var propertyDeclarationSyntax = (PropertyDeclarationSyntax)context.Node;
+
+            if (IsPubliclyExposedIEnumerableTypes(context)) { return; }
+
+            context.ReportDiagnostic(Diagnostic.Create(Descriptor, propertyDeclarationSyntax.GetLocation()));
+        }
+
+        private bool IsPubliclyExposedIEnumerableTypes(SyntaxNodeAnalysisContext context)
+        {
+            INamedTypeSymbol namedTypeSymbol = null;
+            bool isArray = false;
+            if (context.ContainingSymbol.Kind == SymbolKind.Method)
+            {
+                IMethodSymbol method = (IMethodSymbol)context.ContainingSymbol;
+                isArray = method.ReturnType is IArrayTypeSymbol;
+                namedTypeSymbol = method.ReturnType as INamedTypeSymbol;
+                
+            }
+            else if (context.ContainingSymbol.Kind == SymbolKind.Property)
+            {
+                IPropertySymbol property = (IPropertySymbol)context.ContainingSymbol;
+                isArray = property is IArrayTypeSymbol;
+                namedTypeSymbol = property.Type as INamedTypeSymbol;
+                
+            }
+
+            if (isArray ||
+                    (namedTypeSymbol != null
+                     && namedTypeSymbol.ContainingNamespace.ToDisplayString().StartsWith("System.Collections")
+                     && namedTypeSymbol.ConstructedFrom.Interfaces.Any(x => x.ToDisplayString() == "System.Collections.IEnumerable")
+                     && namedTypeSymbol.ConstructedFrom.ToDisplayString() != "string"))
+            {
+                var fullName = namedTypeSymbol.OriginalDefinition.ToDisplayString();
+                if (fullName == "System.Collections.Generic.ISet<T>"
+                    || fullName == "System.Collections.Generic.IList<T>"
+                    || fullName == "System.Collections.Generic.IDictionary<TKey, TValue>"
+                    || fullName == "System.Collections.Generic.IReadOnlyDictionary<TKey, TValue>"
+                    || fullName == "System.Collections.Generic.KeyedCollection<TKey, TValue>")
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
