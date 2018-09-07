eef3c3e55b9e7311d654f723c9c99ac14113d86e
Mike Chamberlain <mike.chamberlain@agoda.com>
2018-09-07 14:54:57 +0700

    Refactoring helpful base classes for common analyzer scenarios

---
+++ src/Agoda.Analyzers/AgodaCustom/ForbiddenInvocationRule.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Agoda.Analyzers.AgodaCustom
+{
+    public class ForbiddenInvocationRule
+    {
+        public string NamespaceAndType { get; }
+        public Regex ForbiddenIdentifierNameRegex { get; }
+
+        private ForbiddenInvocationRule(string namespaceAndType, Regex forbiddenIdentifierNameRegex)
+        {
+            NamespaceAndType = namespaceAndType;
+            ForbiddenIdentifierNameRegex = forbiddenIdentifierNameRegex;
+        }
+
+        public static ForbiddenInvocationRule Create(string namespaceAndType, Regex forbiddenMethodNameRegex)
+            => new ForbiddenInvocationRule(namespaceAndType, forbiddenMethodNameRegex);
+    }
+}
