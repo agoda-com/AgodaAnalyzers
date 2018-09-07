094be186be6ab28c429c0a02798057a452d19ad0
Batushkov, Vlad (Agoda) <vlad.batushkov@agoda.com>
2018-09-07 11:19:02 +0700

    TestCases for unit-tests

--- src/Agoda.Analyzers.Test/AgodaCustom/AG0026UnitTests.cs
+++ src/Agoda.Analyzers.Test/AgodaCustom/AG0026UnitTests.cs
@@ -10,6 +10,7 @@ using Microsoft.CodeAnalysis;
 using OpenQA.Selenium;
 using Agoda.Analyzers.AgodaCustom;
 using System.Collections.ObjectModel;
+using OpenQA.Selenium.Chrome;
 
 namespace Agoda.Analyzers.Test.AgodaCustom
 {
@@ -21,40 +22,28 @@ namespace Agoda.Analyzers.Test.AgodaCustom
         }
 
         [Test]
-        public async Task AG0026_WhenUsedForbiddenFindMethod_ThenShowWarning()
+        [TestCase("FindElementByClassName")]
+        [TestCase("FindElementById")]
+        [TestCase("FindElementByLinkText")]
+        [TestCase("FindElementByName")]
+        [TestCase("FindElementByPartialLinkText")]
+        [TestCase("FindElementByTagName")]
+        [TestCase("FindElementByXPath")]
+        public async Task AG0026_WhenUsedForbiddenFindElementMethod_ThenShowWarning(string methodName)
         {
-            var testCode = @"
+            var testCode = $@"
             using System;
             using OpenQA.Selenium;
             using OpenQA.Selenium.Chrome;
             using System.Collections.ObjectModel;
 
             namespace Selenium.Tests.Utils
-            {
+            {{
                 public class Utils
-                {
-                    public IWebElement element11 => new ChromeDriver().FindElementByClassName(""classname"");
-                    public ReadOnlyCollection<IWebElement> element12 => new ChromeDriver().FindElementsByClassName(""classname"");
-
-                    public IWebElement element21 => new ChromeDriver().FindElementById(""id"");
-                    public ReadOnlyCollection<IWebElement> element22 => new ChromeDriver().FindElementsById(""id"");
-
-                    public IWebElement element31 => new ChromeDriver().FindElementByLinkText(""linktext"");
-                    public ReadOnlyCollection<IWebElement> element32 => new ChromeDriver().FindElementsByLinkText(""linktext"");
-
-                    public IWebElement element41 => new ChromeDriver().FindElementByName(""name"");
-                    public ReadOnlyCollection<IWebElement> element42 => new ChromeDriver().FindElementsByName(""name"");
-
-                    public IWebElement element51 => new ChromeDriver().FindElementByPartialLinkText(""plt"");
-                    public ReadOnlyCollection<IWebElement> element52 => new ChromeDriver().FindElementsByPartialLinkText(""plt"");
-
-                    public IWebElement element61 => new ChromeDriver().FindElementByTagName(""tagname"");
-                    public ReadOnlyCollection<IWebElement> element62 => new ChromeDriver().FindElementsByTagName(""tagname"");
-
-                    public IWebElement element71 => new ChromeDriver().FindElementByXPath(""xpath"");
-                    public ReadOnlyCollection<IWebElement> element72 => new ChromeDriver().FindElementsByXPath(""xpath"");
-                }
-            }";
+                {{
+                    public IWebElement element11 => new ChromeDriver().{methodName}(""abc"");
+                }}
+            }}";
 
             var analyzers = GetCSharpDiagnosticAnalyzers().ToImmutableArray();
             var documents = CreateProject(new[] { testCode })
@@ -68,50 +57,72 @@ namespace Agoda.Analyzers.Test.AgodaCustom
             var baseResult = CSharpDiagnostic(AG0026EnsureOnlyCssSelectorIsUsedToFindElements.DIAGNOSTIC_ID);
             VerifyDiagnosticResults(diag, analyzers, new[]
             {
-                baseResult.WithLocation(11, 53),
-                baseResult.WithLocation(12, 73),
-                baseResult.WithLocation(14, 53),
-                baseResult.WithLocation(15, 73),
-                baseResult.WithLocation(17, 53),
-                baseResult.WithLocation(18, 73),
-                baseResult.WithLocation(20, 53),
-                baseResult.WithLocation(21, 73),
-                baseResult.WithLocation(23, 53),
-                baseResult.WithLocation(24, 73),
-                baseResult.WithLocation(26, 53),
-                baseResult.WithLocation(27, 73),
-                baseResult.WithLocation(29, 53),
-                baseResult.WithLocation(30, 73)
+                baseResult.WithLocation(11, 53)
             });
         }
 
         [Test]
-        public async Task AG0026_WhenUsedForbiddenAccessor_ThenShowWarning()
+        [TestCase("FindElementsByClassName")]
+        [TestCase("FindElementsById")]
+        [TestCase("FindElementsByLinkText")]
+        [TestCase("FindElementsByName")]
+        [TestCase("FindElementsByPartialLinkText")]
+        [TestCase("FindElementsByTagName")]
+        [TestCase("FindElementsByXPath")]
+        public async Task AG0026_WhenUsedForbiddenFindElementsMethod_ThenShowWarning(string methodName)
         {
-            var testCode = @"
+            var testCode = $@"
+            using System;
+            using OpenQA.Selenium;
+            using OpenQA.Selenium.Chrome;
+            using System.Collections.ObjectModel;
+
+            namespace Selenium.Tests.Utils
+            {{
+                public class Utils
+                {{
+                    public ReadOnlyCollection<IWebElement> elements => new ChromeDriver().{methodName}(""abc"");
+                }}
+            }}";
+
+            var analyzers = GetCSharpDiagnosticAnalyzers().ToImmutableArray();
+            var documents = CreateProject(new[] { testCode })
+                           .AddMetadataReference(MetadataReference.CreateFromFile(typeof(IWebElement).Assembly.Location))
+                           .AddMetadataReference(MetadataReference.CreateFromFile(typeof(ReadOnlyCollection<>).Assembly.Location))
+                           .Documents
+                           .ToArray();
+
+            var diag = await GetSortedDiagnosticsFromDocumentsAsync(analyzers, documents, CancellationToken.None).ConfigureAwait(false);
+
+            var baseResult = CSharpDiagnostic(AG0026EnsureOnlyCssSelectorIsUsedToFindElements.DIAGNOSTIC_ID);
+            VerifyDiagnosticResults(diag, analyzers, new[]
+            {
+                baseResult.WithLocation(11, 72)
+            });
+        }
+        
+        [Test]
+        [TestCase("ClassName")]
+        [TestCase("Id")]
+        [TestCase("LinkText")]
+        [TestCase("Name")]
+        [TestCase("PartialLinkText")]
+        [TestCase("TagName")]
+        [TestCase("XPath")]
+        public async Task AG0026_WhenUsedForbiddenAccessor_ThenShowWarning(string methodName)
+        {
+            var testCode = $@"
             using System;
             using OpenQA.Selenium;
             using OpenQA.Selenium.Chrome;
 
             namespace Selenium.Tests.Utils
-            {
+            {{
                 public class Utils
-                {
-                    public IWebElement element11 => new ChromeDriver().FindElement(By.ClassName(""classname""));
-
-                    public IWebElement element21 => new ChromeDriver().FindElement(By.Id(""id""));
-
-                    public IWebElement element31 => new ChromeDriver().FindElement(By.LinkText(""linktext""));
-
-                    public IWebElement element41 => new ChromeDriver().FindElement(By.Name(""name""));
-
-                    public IWebElement element51 => new ChromeDriver().FindElement(By.PartialLinkText(""plt""));
-
-                    public IWebElement element61 => new ChromeDriver().FindElement(By.TagName(""tagname""));
-
-                    public IWebElement element71 => new ChromeDriver().FindElement(By.XPath(""xpath""));
-                }
-            }";
+                {{
+                    public IWebElement element11 => new ChromeDriver().FindElement(By.{methodName}(""abc""));
+                }}
+            }}";
 
             var analyzers = GetCSharpDiagnosticAnalyzers().ToImmutableArray();
             var documents = CreateProject(new[] { testCode })
@@ -124,18 +135,12 @@ namespace Agoda.Analyzers.Test.AgodaCustom
             var baseResult = CSharpDiagnostic(AG0026EnsureOnlyCssSelectorIsUsedToFindElements.DIAGNOSTIC_ID);
             VerifyDiagnosticResults(diag, analyzers, new[]
             {
-                baseResult.WithLocation(10, 84),
-                baseResult.WithLocation(12, 84),
-                baseResult.WithLocation(14, 84),
-                baseResult.WithLocation(16, 84),
-                baseResult.WithLocation(18, 84),
-                baseResult.WithLocation(20, 84),
-                baseResult.WithLocation(22, 84)
+                baseResult.WithLocation(10, 84)
             });
         }
 
         [Test]
-        public async Task AG0026_WhenUsedNotForbiddenFindMethod_ThenNoWarning()
+        public async Task AG0026_WhenUsedNotForbiddenMethod_ThenNoWarning()
         {
             var testCode = @"
             using System;
@@ -149,6 +154,7 @@ namespace Agoda.Analyzers.Test.AgodaCustom
                 {
                     public IWebElement element1 => new ChromeDriver().FindElementByCssSelector(""selector"");
                     public ReadOnlyCollection<IWebElement> element12 => new ChromeDriver().FindElementsByCssSelector(""selector"");
+                    public object obj => new ChromeDriver().ExecuteScript(""scrpt"");
                 }
             }";
 
