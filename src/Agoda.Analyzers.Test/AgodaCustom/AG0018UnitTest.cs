1d49d902f963222ec87dd05a6f06a5df4bef2a52
Susakpol <susakpol.kunvuttivanit@agoda.com>
2018-09-10 23:23:14 +0700

    fix code review

--- src/Agoda.Analyzers.Test/AgodaCustom/AG0018UnitTest.cs
+++ src/Agoda.Analyzers.Test/AgodaCustom/AG0018UnitTest.cs
@@ -18,28 +18,25 @@ namespace Agoda.Analyzers.Test.AgodaCustom
     public class AG0018UnitTest : DiagnosticVerifier
     {
         [Test]
-        public async Task AG0018_WhenCreateAMethodWhichReturnList_ShouldReturnAsAnInterface()
+        public async Task AG0018_ShouldBeAllowedWhenCreateAMethodWhichReturnInterfaces()
         {
-            var code = $@"
-                        namespace Tests
-                        {{
+            var code = @"namespace Tests
+                        {
                             using System.Collections.Generic;
                             using System.Collections.ObjectModel;
-                            public class IServerNamesProvider
-                            {{
-                                public List<double> LocalServer {{ get; set; }}
-                                public IList<double> LocalServerV2 {{ get; set; }}
-                                public HashSet<int> GetServerSet() {{ return null; }}
-                                public ISet<int> GetServerSetV2() {{ return null; }}
-                                public List<int> GetServerCounts() {{ return null; }}
-                                public IList<string> GetServerFullNames() {{ return null; }}        
-                                public Dictionary<string, string> GetServerDns() {{ return null; }}
-                                public IDictionary<string, string> GetServerDnsV2() {{ return null; }}
-                                public ReadOnlyDictionary<string, int> GetNumberOfServer() {{ return null; }}        
-                                public IReadOnlyDictionary<string, int> GetNumberOfServerV2() {{ return null; }}
-                                public KeyedCollection<string, string> GetServerDnsV3() {{ return null; }}
-                            }}
-                        }}";
+                            public class ServerNamesProvider
+                            {
+                                public IList<double> LocalServerV2 { get; set; }
+                                public ISet<int> GetServerSetV2() { return null; }
+                                public IList<string> GetServerFullNames() { return null; }        
+                                public IDictionary<string, string> GetServerDnsV2() { return null; }
+                                public IReadOnlyDictionary<string, int> GetNumberOfServerV2() { return null; }
+                                public KeyedCollection<string, string> GetServerDnsV3() { return null; }
+                                public int NumberOfServer { get; set; }
+                                public byte[] RawData { get; set; }
+                                public string DNSName { get; set; }
+                            }
+                        }";
 
             var nUnit = MetadataReference.CreateFromFile(typeof(TestFixtureAttribute).Assembly.Location);
 
@@ -51,21 +48,131 @@ namespace Agoda.Analyzers.Test.AgodaCustom
             var analyzersArray = GetCSharpDiagnosticAnalyzers().ToImmutableArray();
 
             var diag = await GetSortedDiagnosticsFromDocumentsAsync(analyzersArray, new[] { doc }, CancellationToken.None).ConfigureAwait(false);
-            var baseResult = CSharpDiagnostic(AG0018EnsureThatPubliclyExposedIEnumerableTypes.DiagnosticId);
+            var baseResult = CSharpDiagnostic(AG0018PermitOnlyCertainPubliclyExposedEnumerables.DiagnosticId);
+
+            VerifyDiagnosticResults(diag, analyzersArray, new DiagnosticResult[] { });
+        }
+
+        [Test]
+        public async Task AG0018_ShouldNotBeAllowedWhenCreateAMethodWhichReturnImplementedClass()
+        {
+            var code = @"namespace Tests
+                        {
+                            using System.Collections.Generic;
+                            using System.Collections.ObjectModel;
+                            public class ServerNamesProvider
+                            {
+                                public List<double> LocalServer { get; set; }
+                                public HashSet<int> GetServerSet() { return null; }
+                                public List<int> GetServerCounts() { return null; }
+                                public Dictionary<string, string> GetServerDns() { return null; }
+                                public ReadOnlyDictionary<string, int> GetNumberOfServer() { return null; }  
+                                public int[] ServerPerCluster { get; set; }
+                            }
+                        }";
+
+            var nUnit = MetadataReference.CreateFromFile(typeof(TestFixtureAttribute).Assembly.Location);
+
+            var doc = CreateProject(new[] { code })
+                .AddMetadataReference(nUnit)
+                .Documents
+                .First();
+
+            var analyzersArray = GetCSharpDiagnosticAnalyzers().ToImmutableArray();
+
+            var diag = await GetSortedDiagnosticsFromDocumentsAsync(analyzersArray, new[] { doc }, CancellationToken.None).ConfigureAwait(false);
+            var baseResult = CSharpDiagnostic(AG0018PermitOnlyCertainPubliclyExposedEnumerables.DiagnosticId);
 
             VerifyDiagnosticResults(diag, analyzersArray, new[]
             {
+                baseResult.WithLocation(7, 33),
                 baseResult.WithLocation(8, 33),
+                baseResult.WithLocation(9, 33),
                 baseResult.WithLocation(10, 33),
+                baseResult.WithLocation(11, 33),
                 baseResult.WithLocation(12, 33),
-                baseResult.WithLocation(14, 33),
-                baseResult.WithLocation(16, 33),
+            });
+        }
+
+        [Test]
+        public async Task AG0018_WhenCreateAMethodInAnInterfaceWhichReturnList_ShouldReturnAsAnInterface()
+        {
+            var code = @"namespace Tests
+                        {
+                            using System.Collections.Generic;
+                            using System.Collections.ObjectModel;
+                            public interface IServerNamesProvider
+                            {
+                                HashSet<int> GetServerSet();
+                                ISet<int> GetServerSetV2();
+                                List<int> GetServerCounts();
+                                IList<string> GetServerFullNames();      
+                                Dictionary<string, string> GetServerDns();
+                                IDictionary<string, string> GetServerDnsV2();
+                                ReadOnlyDictionary<string, int> GetNumberOfServer();
+                                IReadOnlyDictionary<string, int> GetNumberOfServerV2();
+                                KeyedCollection<string, string> GetServerDnsV3();
+                            }
+                        }";
+
+            var nUnit = MetadataReference.CreateFromFile(typeof(TestFixtureAttribute).Assembly.Location);
+
+            var doc = CreateProject(new[] { code })
+                .AddMetadataReference(nUnit)
+                .Documents
+                .First();
+
+            var analyzersArray = GetCSharpDiagnosticAnalyzers().ToImmutableArray();
+
+            var diag = await GetSortedDiagnosticsFromDocumentsAsync(analyzersArray, new[] { doc }, CancellationToken.None).ConfigureAwait(false);
+            var baseResult = CSharpDiagnostic(AG0018PermitOnlyCertainPubliclyExposedEnumerables.DiagnosticId);
+
+            VerifyDiagnosticResults(diag, analyzersArray, new[]
+            {
+                baseResult.WithLocation(7, 33),
+                baseResult.WithLocation(9, 33),
+                baseResult.WithLocation(11, 33),
+                baseResult.WithLocation(13, 33),
+            });
+        }
+
+        [Test]
+        public async Task AG0018_WhenCreateAMethodWhichReturnList_ShouldNotEffectOtherCases()
+        {
+            var code = @"namespace Tests
+                        {
+                            using System.Collections.Generic;
+                            using System.Collections.ObjectModel;
+                            public class ServerNamesProvider
+                            {
+                                public List<double> LocalServer { get; set; }
+                                private List<int> GetIPFromFile() { return null; }
+                                protected List<int> GetIPFromLocalFile() { return null; }
+                                internal List<int> GetIPFromLocalDisk() { return null; }
+                            }
+                        }";
+
+            var nUnit = MetadataReference.CreateFromFile(typeof(TestFixtureAttribute).Assembly.Location);
+
+            var doc = CreateProject(new[] { code })
+                .AddMetadataReference(nUnit)
+                .Documents
+                .First();
+
+            var analyzersArray = GetCSharpDiagnosticAnalyzers().ToImmutableArray();
+
+            var diag = await GetSortedDiagnosticsFromDocumentsAsync(analyzersArray, new[] { doc }, CancellationToken.None).ConfigureAwait(false);
+            var baseResult = CSharpDiagnostic(AG0018PermitOnlyCertainPubliclyExposedEnumerables.DiagnosticId);
+
+            VerifyDiagnosticResults(diag, analyzersArray, new[]
+            {
+                baseResult.WithLocation(7, 33),
             });
         }
 
         protected override IEnumerable<DiagnosticAnalyzer> GetCSharpDiagnosticAnalyzers()
         {
-            yield return new AG0018EnsureThatPubliclyExposedIEnumerableTypes();
+            yield return new AG0018PermitOnlyCertainPubliclyExposedEnumerables();
         }
     }
 }
