e91d6fb0c3411a90efdc4e3ee6fe9579fc01f075
Methavee Pakpinyo <methavee.pakpinyo@agoda.com>
2018-09-12 17:34:29 +0700

    complete analyser for prevent inheritance on testfixture.

---
+++ src/Agoda.Analyzers.Test/AgodaCustom/AG0010UnitTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Agoda.Analyzers.AgodaCustom;
+using Agoda.Analyzers.Test.Helpers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using NUnit.Framework;
+
+namespace Agoda.Analyzers.Test.AgodaCustom
+{
+    internal class AG0010UnitTests : DiagnosticVerifier
+    {
+        private const string TEST_FIXTURE_ATTRIBUTE = "TestFixture";
+        private const string TEST_INHERITANCE_CLASSNAME = "BaseTest";
+
+        [Test]
+        public async Task AG0010_WhenNoTestFixtureAttribute_ShouldntShowAnyWarning()
+        {
+            var code = ClassBuilder.New()
+                .WithNamespace()
+                .WithClass()
+                .Build();
+
+            await TestWarnings(code);
+        }
+
+        [Test]
+        public async Task AG0010_WhenPutTestFixtureAttribute_ButNoInheritance_ShouldntShowWarning()
+        {
+            var code = ClassBuilder.New()
+                .WithNamespace()
+                .WithClass(className: "test", attribute: TEST_FIXTURE_ATTRIBUTE)
+                .WithAttributeClass(TEST_FIXTURE_ATTRIBUTE)
+                .Build();
+
+            await TestWarnings(code);
+        }
+
+        [Test]
+        public async Task AG0010_WhenPutTestFixtureAttribute_ButHasInheritance_ShouldShowWarning()
+        {
+            var code = ClassBuilder.New()
+                .WithNamespace()
+                .WithClass(className:"test",attribute: TEST_FIXTURE_ATTRIBUTE,inheritanceClassName:TEST_INHERITANCE_CLASSNAME)
+                .WithAttributeClass(TEST_FIXTURE_ATTRIBUTE)
+                .WithInheritanceClass(TEST_INHERITANCE_CLASSNAME)
+                .Build();
+
+            var baseResult =
+                CSharpDiagnostic(AG0010PreventTestFixtureInheritance.DiagnosticId);
+            var expected = new[]
+            {
+                baseResult.WithLocation(4, 2)
+            };
+            await TestWarnings(code, expected);
+        }
+
+        private async Task TestWarnings(string code, DiagnosticResult[] expected = null)
+        {
+            expected = expected ?? new DiagnosticResult[0];
+            var doc = CreateProject(new[] { code })
+                .Documents
+                .First();
+
+            var analyzersArray = GetCSharpDiagnosticAnalyzers().ToImmutableArray();
+
+            var diag = await GetSortedDiagnosticsFromDocumentsAsync(analyzersArray, new[] { doc }, CancellationToken.None)
+                .ConfigureAwait(false);
+
+
+            VerifyDiagnosticResults(diag, analyzersArray, expected);
+        }
+
+        protected override IEnumerable<DiagnosticAnalyzer> GetCSharpDiagnosticAnalyzers()
+        {
+            yield return new AG0010PreventTestFixtureInheritance();
+        }
+    }
+}
---
+++ src/Agoda.Analyzers/AgodaCustom/AG0010PreventTestFixtureInheritance.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Agoda.Analyzers.Helpers;
+
+namespace Agoda.Analyzers.AgodaCustom
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AG0010PreventTestFixtureInheritance : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "AG0010";
+        private static readonly Regex MatchTestAttributeName = new Regex("^TestFixture$");
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(
+            nameof(CustomRulesResources.AG0010Title),
+            CustomRulesResources.ResourceManager,
+            typeof(CustomRulesResources));
+
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(
+            nameof(CustomRulesResources.AG0010Title),
+            CustomRulesResources.ResourceManager,
+            typeof(CustomRulesResources));
+
+        private static readonly LocalizableString Description = DescriptionContentLoader.GetAnalyzerDescription(
+            nameof(AG0010PreventTestFixtureInheritance));
+
+        private static readonly DiagnosticDescriptor Descriptor = new DiagnosticDescriptor(
+            DiagnosticId,
+            Title,
+            MessageFormat,
+            AnalyzerCategory.CustomQualityRules,
+            DiagnosticSeverity.Warning,
+            AnalyzerConstants.EnabledByDefault,
+            Description,
+            null,
+            WellKnownDiagnosticTags.EditAndContinue);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ClassDeclaration);
+        }
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Descriptor);
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var classDeclaration = (ClassDeclarationSyntax)context.Node;
+
+            var hasTestFixtureAttribute = classDeclaration.AttributeLists
+                .SelectMany(al => al.Attributes)
+                .Select(a => a.Name as IdentifierNameSyntax)
+                .Where(name => name != null)
+                .Select(name => name.Identifier.ValueText)
+                .Any(MatchTestAttributeName.IsMatch);
+
+            if (!hasTestFixtureAttribute) return;
+
+            if (classDeclaration.BaseList == null) return;
+
+            context.ReportDiagnostic(Diagnostic.Create(Descriptor, classDeclaration.GetLocation()));
+        }
+    }
+}
