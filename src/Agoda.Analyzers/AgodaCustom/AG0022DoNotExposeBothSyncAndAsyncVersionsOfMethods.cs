c02e1381eb6326a533683e14519dd1a4ea152a7c
Sheep <Supacheep.Amtade@agoda.com>
2018-09-14 16:01:32 +0700

    Enhance normalizing method name

--- src/Agoda.Analyzers/AgodaCustom/AG0022DoNotExposeBothSyncAndAsyncVersionsOfMethods.cs
+++ src/Agoda.Analyzers/AgodaCustom/AG0022DoNotExposeBothSyncAndAsyncVersionsOfMethods.cs
@@ -1,4 +1,5 @@
-﻿using Agoda.Analyzers.Helpers;
+﻿using System;
+using Agoda.Analyzers.Helpers;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -14,24 +15,34 @@ namespace Agoda.Analyzers.AgodaCustom
     {
         public const string DiagnosticId = "AG0022";
 
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(CustomRulesResources.AG0022Title), CustomRulesResources.ResourceManager, typeof(CustomRulesResources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(CustomRulesResources.AG0022MessageFormat), CustomRulesResources.ResourceManager, typeof(CustomRulesResources));
-        private static readonly LocalizableString Description = DescriptionContentLoader.GetAnalyzerDescription(nameof(AG0022DoNotExposeBothSyncAndAsyncVersionsOfMethods));
+        private static readonly LocalizableString Title = new LocalizableResourceString(
+            nameof(CustomRulesResources.AG0022Title), CustomRulesResources.ResourceManager,
+            typeof(CustomRulesResources));
+
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(
+            nameof(CustomRulesResources.AG0022MessageFormat), CustomRulesResources.ResourceManager,
+            typeof(CustomRulesResources));
+
+        private static readonly LocalizableString Description =
+            DescriptionContentLoader.GetAnalyzerDescription(nameof(AG0022DoNotExposeBothSyncAndAsyncVersionsOfMethods));
+
         private static readonly DiagnosticDescriptor Descriptor =
             new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerCategory.CustomQualityRules,
-                DiagnosticSeverity.Warning, AnalyzerConstants.EnabledByDefault, Description, null, WellKnownDiagnosticTags.EditAndContinue);
+                DiagnosticSeverity.Warning, AnalyzerConstants.EnabledByDefault, Description, null,
+                WellKnownDiagnosticTags.EditAndContinue);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Descriptor);
 
         public override void Initialize(AnalysisContext context)
         {
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
             context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.MethodDeclaration);
         }
 
         private void AnalyzeNode(SyntaxNodeAnalysisContext context)
         {
-            var currentMethodDeclaration = (MethodDeclarationSyntax)context.Node;
+            var currentMethodDeclaration = (MethodDeclarationSyntax) context.Node;
             var currentMethodName = currentMethodDeclaration.Identifier.ValueText;
 
             if (!context.Node.Parent.ChildNodes().Any() || "Async".Equals(currentMethodName)) return;
@@ -41,16 +52,16 @@ namespace Agoda.Analyzers.AgodaCustom
                 .Select(node => node.Identifier.ValueText).ToList();
 
             // ensure valid name
-            if (!ExistsBothSyncAndAsyncVersionsOfMethods(currentMethodName, methodNames)) return;
+            if (!CheckSyncAndAsyncMethodNamimg(currentMethodName, methodNames)) return;
 
             context.ReportDiagnostic(Diagnostic.Create(Descriptor, currentMethodDeclaration.GetLocation()));
         }
 
-        private bool ExistsBothSyncAndAsyncVersionsOfMethods(string currentMethodName, IEnumerable<string> methodNames)
-        {
+        private static bool CheckSyncAndAsyncMethodNamimg(string currentMethodName, IEnumerable<string> methodNames)
+        { 
             var compareToMethodName = currentMethodName.EndsWith("Async") ?
-                currentMethodName.Remove(currentMethodName.Length - 6, currentMethodName.Length - 1) :
-                currentMethodName + "Async";
+                currentMethodName.Remove(currentMethodName.IndexOf("Async", StringComparison.Ordinal)) :
+                $"{currentMethodName}Async";
 
             return methodNames.Any(methodName => methodName.Equals(compareToMethodName));
         }
