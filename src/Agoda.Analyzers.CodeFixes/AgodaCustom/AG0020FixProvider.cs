242bd56c7031cb7ee46441d6399d303f3e62bbbc
samtade <40428696+samtade@users.noreply.github.com>
2018-09-18 19:23:56 +0700

    Revert unexpected change

--- src/Agoda.Analyzers.CodeFixes/AgodaCustom/AG0020FixProvider.cs
+++ src/Agoda.Analyzers.CodeFixes/AgodaCustom/AG0020FixProvider.cs
@@ -15,6 +15,7 @@ using Microsoft.CodeAnalysis.Text;
 namespace Agoda.Analyzers.CodeFixes.AgodaCustom
 {
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(AG0020FixProvider))]
+    [Shared]
     public class AG0020FixProvider : CodeFixProvider
     {
         public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(AG0020AvoidReturningNullEnumerables.DIAGNOSTIC_ID);
