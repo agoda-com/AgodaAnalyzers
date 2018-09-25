574d15edd5d8caca64c28b0144548c18b747d006
Mafil <aleksandar_mafilovski@yahoo.com>
2018-09-25 14:54:23 +0700

    Remove AG0024UnitTests.cs and add TestCases (proof of concept)

---
+++ src/Agoda.Analyzers.Test/AgodaCustom/TestCases/AG0024/NoWarning/FactoryStartNewWithLongRunningParameter.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+class FactoryStartNewWithLongRunningParameter
+{
+    public void TestMethod1()
+    {
+        Task.Factory.StartNew(MyMethod, CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+    }
+
+    public void MyMethod()
+    {
+    }
+}
---
+++ src/Agoda.Analyzers.Test/AgodaCustom/TestCases/AG0024/Warning/FactoryStartNewWithMultipleParameters.cs
@@ -0,0 +1,15 @@
+﻿/*9,9*/
+using System.Threading;
+using System.Threading.Tasks;
+
+class FactoryStartNewWithMultipleParameters
+{
+    public void TestMethod1()
+    {
+        Task.Factory.StartNew(MyMethod, CancellationToken.None, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default);
+    }
+
+    public void MyMethod()
+    {
+    }
+}
