bfe48bcdac894a88434022aca7ecb3a26379bb9d
Mike Chamberlain <mike.chamberlain@agoda.com>
2018-09-26 18:52:16 +0700

    Closes #105

--- src/Agoda.Analyzers/AgodaCustom/AG0027EnsureOnlyDataSeleniumIsUsedToFindElements.cs
+++ src/Agoda.Analyzers/AgodaCustom/AG0027EnsureOnlyDataSeleniumIsUsedToFindElements.cs
@@ -58,13 +58,20 @@ namespace Agoda.Analyzers.AgodaCustom
             }
             
             var firstArgument = invocationExpressionSyntax.ArgumentList.Arguments.FirstOrDefault();
+            
             if (firstArgument?.Expression is LiteralExpressionSyntax && !MatchDataSelenium.IsMatch(firstArgument.ToString()))
             {
                 context.ReportDiagnostic(Diagnostic.Create(Descriptor, firstArgument.GetLocation()));
             }
 
-
-
+            if (firstArgument?.Expression is IdentifierNameSyntax)
+            {
+                var constantValue = context.SemanticModel.GetConstantValue(firstArgument.Expression);
+                if (constantValue.HasValue && !MatchDataSelenium.IsMatch($@"""{constantValue.Value}"""))
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Descriptor, firstArgument.GetLocation()));
+                }
+            }
         }
     }
 }
